5a670972be37f8ec51bde6a7742c5dd941880f48
William Scanlon <wjs.scanlon@gmail.com>
2017-12-15 16:58:47 -0500

    Added test pcm and mp3 files

--- test.py
+++ test.py
@@ -2,10 +2,10 @@ from winkrelayintercom import WinkRelayIntercomBroadcaster
 
 # Takes an MP3, converts and boosts the audio sends to broadcaster as raw data
 broadcaster = WinkRelayIntercomBroadcaster("192.168.1.255", convert=True, audio_boost=15)
-with open("somemp3.mp3", "rb") as f:
+with open("test.mp3", "rb") as f:
     data = f.read()
-    broadcaster.send_audio(data=data)
+    broadcaster.send_audio(data)
 
-# Takes a PCM raw audio file and sends it as a file path. No audio boost.
+# # Takes a PCM raw audio file and sends it as a file path. No audio boost.
 broadcaster = WinkRelayIntercomBroadcaster("192.168.1.255", convert=False, audio_boost=None)
-broadcaster.send_audio("rawpcm.pcm")
+broadcaster.send_audio("test.pcm")
