f197169e15a4b2d7d02f6bf673c72258de7abea1
William Scanlon <wjs.scanlon@gmail.com>
2017-12-20 20:53:40 -0500

    Updated the test script

--- test.py
+++ test.py
@@ -1,11 +1,17 @@
+import time
+
 from winkrelayintercom import WinkRelayIntercomBroadcaster
 
 # Takes an MP3, converts and boosts the audio sends to broadcaster as raw data
-#broadcaster = WinkRelayIntercomBroadcaster("192.168.1.255", convert=True, audio_boost=15)
-#with open("test.mp3", "rb") as f:
-#    data = f.read()
-#    broadcaster.send_audio(data)
+broadcaster = WinkRelayIntercomBroadcaster("192.168.1.255", convert=True, audio_boost=15)
+with open("test.mp3", "rb") as f:
+   data = f.read()
+   broadcaster.send_audio(data)
 
 # # Takes a PCM raw audio file and sends it as a file path. No audio boost.
-broadcaster = WinkRelayIntercomBroadcaster("192.168.5.255", convert=False, audio_boost=10)
+broadcaster = WinkRelayIntercomBroadcaster("192.168.1.255", convert=False, audio_boost=10)
+# Wait for the relay to send out an SSDP request (Not sure how long that takes)
+time.sleep(120)
 broadcaster.send_audio("test.pcm")
+# Kill the SSDP responder
+broadcaster.stop()
